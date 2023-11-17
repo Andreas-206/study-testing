4d220d463a18135c12385a7b7df5b06e70a5d231
Andreas-206 <puma_real@ukr.net>
2023-11-17 18:00:39 +0200

    avto

--- testing.js
+++ testing.js
@@ -1,17 +1,39 @@
-function changeEven(numbers, value) {
-  const updatedNumbers = [];
+// function changeEven(numbers, value) {
+//   const updatedNumbers = [];
 
-  for (let i = 0; i < numbers.length; i++) {
-    if (numbers[i] % 2 === 0) {
-      updatedNumbers.push(numbers[i] + value);
-    } else {
-      updatedNumbers.push(numbers[i]);
-    }
+//   for (let i = 0; i < numbers.length; i++) {
+//     if (numbers[i] % 2 === 0) {
+//       updatedNumbers.push(numbers[i] + value);
+//     } else {
+//       updatedNumbers.push(numbers[i]);
+//     }
+//   }
+
+//   return updatedNumbers
+// }
+// console.log(changeEven)
+
+class Car {
+  #brand;
+  constructor({ brand, model, price }) {
+    this.#brand = brand;
+    this.model = model;
+    this.price = price;
   }
 
-  return updatedNumbers
+  getPrice() {
+    return this.price;
+  }
+
+  changePrice(newPrice) {
+    this.price = newPrice;
+  }
+
+  getBrand() {
+    return this.#brand;
+  }
+
+  changeBrand(newBrand) {
+    this.#brand = newBrand;
+  }
 }
-console.log(changeEven)
-
-
-
