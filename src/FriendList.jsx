a34b93834c713cb378fe229fa73e16360face1d7
Andreas-206 <puma_real@ukr.net>
2024-02-15 15:58:30 +0200

    +

--- src/FriendList.jsx
+++ src/FriendList.jsx
@@ -1,14 +1,29 @@
-import FriendListItem from '../FriendListItem/FriendListItem'
+import css from './FriendList.module.css'
+import FriendListItem from '../FriendListItem/FriendListItem.jsx'
+import PropTypes from 'prop-types'
 
 const FriendList = ({ friends }) => {
-	return (
-		<ul>
-			{/* Кількість li залежить від кількості об'єктів в масиві */}
-			<li>
-				<FriendListItem />
-			</li>
-		</ul>
-	)
+	if (friends.length > 0) {
+		return (
+			<ul className={css.list}>
+				{friends.map(({ id, avatar, name, isOnline }) => (
+					<li className={css.item} key={id}>
+						<FriendListItem avatar={avatar} name={name} isOnline={isOnline} />
+					</li>
+				))}
+			</ul>
+		)
+	} else console.error(Error)
 }
 
-export default FriendList
+FriendList.propTypes = {
+	friends: PropTypes.arrayOf(
+		PropTypes.shape({
+			id: PropTypes.number.isRequired,
+			avatar: PropTypes.string,
+			name: PropTypes.string,
+			isOnline: PropTypes.bool,
+		})
+	),
+}
+export default FriendList;
